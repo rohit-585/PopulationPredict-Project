feaf5d508b825a7f85751ddd1f23dbfad9ad55ac
rohit-585 <csjma23001390324@csjmu.ac.in>
2026-09-01 12:11:33 +0530

    Use psycopg-binary and remove apt-get from build script

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install system dependencies for psycopg2
-apt-get update
-apt-get install -y libpq-dev gcc
-
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
