2a24bdf7b674d09d2d58178b471e8345b881a67c
rohit-585 <csjma23001390324@csjmu.ac.in>
2026-09-01 12:10:15 +0530

    Add system dependencies for psycopg2 compilation

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install system dependencies for psycopg2
+apt-get update
+apt-get install -y libpq-dev gcc
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
